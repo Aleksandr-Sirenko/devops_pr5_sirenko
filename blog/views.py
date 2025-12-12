f83ede5f759f29b9a0eafa8f7b401aacac3d646f
Aleksandr-Sirenko <aleksandr.sirenko3@gmail.com>
2025-12-12 08:43:49 +0200

    include comments

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
 from django.core.mail import send_mail
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -28,7 +30,17 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активних коментарів до цього посту
+    comments = post.comments.filter(active=True)
+    
+    # Форма для коментування користувачами
+    form = CommentForm()
+
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
+
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -53,4 +65,17 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
 
